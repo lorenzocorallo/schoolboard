4d15a1940458ecc57cd19039cbf7c613359e5755
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2023-01-22 19:57:58 +0100

    cleanup

---
+++ src/Router.tsx
@@ -0,0 +1,9 @@
+function Router() {
+    return (
+        <div>
+            <p>Hi bro!</p>
+        </div>
+    );
+}
+
+export default Router;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import Router from "./Router";
 import "./style.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Router />
+    </React.StrictMode>
 );
